5a2e61b4b1932a1eb2181959f6113fae212dd12b
Uchencho <aloziekelechi17@gmail.com>
2020-10-07 17:36:17 +0100

    moved the server logic from the main file

--- main.go
+++ main.go
@@ -1,210 +1,16 @@
 package main
 
 import (
-	"bytes"
 	"log"
 	"net/http"
-	"time"
 
-	"github.com/gorilla/websocket"
+	"github.com/Uchencho/telegram/server"
 )
 
 const (
 	// default server address
 	defaultServerAddress = "127.0.0.1:8000"
-
-	// Time allowed to write a message to the peer
-	writeWait = 10 * time.Second
-
-	// Time allowed to read the next pong message to the peer
-	pongWait = 60 * time.Second
-
-	// Send pings to the peer with this period. Must be less than pongWait
-	pingPeriod = (pongWait * 9) / 10
-
-	// Maximum message size allowed from peer
-	maxMessageSize = 512
 )
-
-var (
-	newline = []byte(`\n`)
-	space   = []byte(` `)
-)
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-
-type Client struct {
-	hub *Hub
-
-	// Websocket connection
-	conn *websocket.Conn
-
-	// Buffered channnel of outbound messages
-	send chan []byte
-}
-
-type Hub struct {
-
-	// Registered clients
-	clients map[*Client]bool
-
-	// Inbound messages from the clients
-	broadcast chan []byte
-
-	// Register requests from the clients
-	register chan *Client
-
-	// Unregister requests from clients
-	unregister chan *Client
-}
-
-// readPump pumps messages from the websocket connection to the hub
-func (c *Client) readPump() {
-	defer func() {
-		// I believe this unregister the client and closes the connection
-		c.hub.unregister <- c
-		c.conn.Close()
-
-	}()
-
-	c.conn.SetReadLimit(maxMessageSize)
-	err := c.conn.SetReadDeadline(time.Now().Add(pongWait))
-	if err != nil {
-		log.Println(err)
-	}
-	c.conn.SetPongHandler(func(string) error {
-		err = c.conn.SetReadDeadline(time.Now().Add(pongWait))
-		if err != nil {
-			log.Println(err)
-		}
-		return nil
-	})
-
-	for {
-		_, message, err := c.conn.ReadMessage()
-		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Println("error: ", err)
-			}
-			break
-		}
-		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
-		c.hub.broadcast <- message
-	}
-}
-
-// writePump pumps messages from the hub to the websocket connection
-func (c *Client) writePump() {
-	ticker := time.NewTicker(pingPeriod)
-	defer func() {
-		ticker.Stop()
-		c.conn.Close()
-	}()
-
-	for {
-		select {
-		case message, ok := <-c.send:
-			err := c.conn.SetWriteDeadline(time.Now().Add(writeWait))
-			if err != nil {
-				log.Println(err)
-			}
-			if !ok {
-				err = c.conn.WriteMessage(websocket.CloseMessage, []byte{})
-				if err != nil {
-					log.Println(err)
-				}
-				return
-			}
-
-			w, err := c.conn.NextWriter(websocket.TextMessage)
-			if err != nil {
-				return
-			}
-			_, err = w.Write(message)
-			if err != nil {
-				log.Println(err)
-			}
-
-			// Add queued chat messages to the current websocket message
-			n := len(c.send)
-			for i := 0; i < n; i++ {
-				_, err = w.Write(newline)
-				if err != nil {
-					log.Println(err)
-				}
-
-				_, err = w.Write(<-c.send)
-				if err != nil {
-					log.Println(err)
-				}
-			}
-
-			if err := w.Close(); err != nil {
-				log.Println(err)
-				return
-			}
-
-		case <-ticker.C:
-			err := c.conn.SetWriteDeadline(time.Now().Add(writeWait))
-			if err != nil {
-				log.Println(err)
-			}
-			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
-				return
-			}
-		}
-	}
-}
-
-func newHub() *Hub {
-	return &Hub{
-		broadcast:  make(chan []byte),
-		register:   make(chan *Client),
-		unregister: make(chan *Client),
-		clients:    make(map[*Client]bool),
-	}
-}
-
-func (h *Hub) run() {
-	for {
-		select {
-		case client := <-h.register:
-			h.clients[client] = true
-		case client := <-h.unregister:
-			if _, ok := h.clients[client]; ok {
-				delete(h.clients, client)
-				close(client.send)
-			}
-		case message := <-h.broadcast:
-			for client := range h.clients {
-				select {
-				case client.send <- message:
-				default:
-					close(client.send)
-					delete(h.clients, client)
-				}
-			}
-		}
-	}
-}
-
-// serveWs handles websocket requests from peer? (what is peer)
-func serveWs(hub *Hub, w http.ResponseWriter, req *http.Request) {
-	conn, err := upgrader.Upgrade(w, req, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
-	client.hub.register <- client
-
-	go client.writePump()
-	go client.readPump()
-}
 
 func serveHome(w http.ResponseWriter, req *http.Request) {
 	log.Println(req.URL)
@@ -223,12 +29,12 @@ func serveHome(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 
-	hub := newHub()
-	go hub.run()
+	hub := server.NewHub()
+	go hub.Run()
 
 	http.HandleFunc("/", serveHome)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, req *http.Request) {
-		serveWs(hub, w, req)
+		server.ChatServer(hub, w, req)
 	})
 
 	if err := http.ListenAndServe(defaultServerAddress, nil); err != http.ErrServerClosed {
