33378fe7d19c9caaf584017de163ae1f63a38530
Uchencho <aloziekelechi17@gmail.com>
2020-10-16 18:01:37 +0100

    connecting to mysql database

---
+++ auth/auth.go
@@ -0,0 +1,65 @@
+package auth
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var (
+	signingKey = []byte(os.Getenv("SIGNING_KEY"))
+	// refreshSigningKey = []byte(os.Getenv("REFRESH_SIGNING_KEY"))
+)
+
+func checkAccessToken(accessToken string) (interface{}, error) {
+	token, err := jwt.Parse(accessToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("An error occured")
+		}
+		return signingKey, nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	if cliams, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return cliams["client"], nil
+	}
+	return "", errors.New("Credentials not provided")
+}
+
+func UserMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		if req.Header["Authorization"] != nil {
+			if len(strings.Split(req.Header["Authorization"][0], " ")) < 2 {
+				w.WriteHeader(http.StatusForbidden)
+				fmt.Fprint(w, `{"error" : "Invalid token format"}`)
+				return
+			}
+
+			accessToken := strings.Split(req.Header["Authorization"][0], " ")[1]
+			_, err := checkAccessToken(accessToken)
+			if err != nil && "Token is expired" == err.Error() {
+				w.WriteHeader(http.StatusUnauthorized)
+				fmt.Fprint(w, `{"error" : "Token has expired please login"}`)
+				return
+			} else if err != nil {
+				w.WriteHeader(http.StatusUnauthorized)
+				fmt.Fprint(w, `{"error" : "Invalid token"}`)
+				return
+			}
+
+			next.ServeHTTP(w, req)
+			return
+		}
+		w.WriteHeader(http.StatusForbidden)
+		fmt.Fprint(w, `{"error" : "Token not passed"}`)
+	})
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/Uchencho/telegram
+
+go 1.14
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/websocket v1.4.2
+	github.com/joho/godotenv v1.3.0
+)
